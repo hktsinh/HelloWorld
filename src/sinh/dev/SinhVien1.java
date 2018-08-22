3ed4d289f53bcbf02a458c3726ce662287eec0b8
sinh <hkhactiensinh147@gmail.com>
2018-08-22 16:30:07 +0800

    sinh vien

---
+++ src/sinh/dev/SinhVien1.java
@@ -0,0 +1,12 @@
+package sinh.dev;
+
+public class SinhVien1 {
+public static void main (String[]agrs) {
+	sinhvien sv=new sinhvien();
+	sv.hoTen="Hong Khac Tien Sinh";
+    sv.Tuoi= 20;
+    sv.MaSV = 124;
+    sv.Thi();
+    sv.DongHocPhi();
+ }
+}
